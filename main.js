72117dc8f798e98771cc3f70e4eabecb770dc72d
Taufiqur Rochim <t.rochim@yahoo.com>
2019-11-28 08:51:58 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+//Document Object Model (DOM)
+
+// Single element selector
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('h1'));
+
+
+// Multiple element selector
